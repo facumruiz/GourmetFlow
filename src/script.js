1870e2ed332473f1bcc11a3f694bade490ce7d7b
facumruiz <facundoruiz105@gmail.com>
2023-10-31 07:26:19 -0300

    add validation form registrarMesa()

--- src/script.js
+++ src/script.js
@@ -1,17 +1,56 @@
 // Array para almacenar la información de las mesas registradas
 const mesasRegistradas = [];
 
+// Función para verificar si un número de mesa ya existe
+function mesaExistente(numeroMesa) {
+    return mesasRegistradas.some(mesa => mesa.numero === numeroMesa);
+}
+
+// Función para mostrar un mensaje de error
+function showError(message) {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = message;
+}
+
+// Función para borrar el mensaje de error
+function clearError() {
+    const errorMessage = document.getElementById('error-message');
+    errorMessage.textContent = '';
+}
+
 // Función para registrar una nueva mesa
 function registrarMesa() {
     // Obtener valores del formulario
     const numeroMesa = document.getElementById('numeroMesa').value;
     const disponibilidad = document.getElementById('disponibilidad').value;
 
-    // Validar que la disponibilidad sea un número
-    if (isNaN(disponibilidad)) {
-        alert('Por favor, ingrese un número válido para la disponibilidad.');
+    // Validar que los campos no estén en blanco
+    if (numeroMesa.trim() === '' || disponibilidad.trim() === '') {
+        showError('Por favor, complete todos los campos.');
         return;
     }
+
+
+    // Validar que el número de mesa sea único
+    if (mesaExistente(numeroMesa)) {
+        showError('El número de mesa ya existe. Por favor, ingrese uno diferente.');
+        return;
+    }
+
+    // Validar que la numeroMesa sea un número no negativo
+    if (isNaN(numeroMesa) || numeroMesa < 0) {
+        showError('Por favor, ingrese números válidos para el número de mesa');
+        return;
+    }
+
+    // Validar que la disponibilidad sea un número no negativo
+    if (isNaN(disponibilidad) || disponibilidad < 0) {
+        showError('Por favor, ingrese un número válido y no negativo para la disponibilidad.');
+        return;
+    }
+
+    // Limpiar el mensaje de error si la entrada es válida
+    clearError();
 
     // Agregar la mesa al array de mesas registradas
     mesasRegistradas.push({ numero: numeroMesa, disponibilidad, estado: 'libre', comandas: [] });
@@ -20,6 +59,7 @@ function registrarMesa() {
     limpiarFormulario();
     mostrarMesasGuardadas();
 }
+
 
 // Función para limpiar los campos del formulario
 function limpiarFormulario() {
@@ -96,18 +136,20 @@ function mostrarMesasGuardadas() {
         btnMesa.textContent = `Mesa ${mesa.numero} - Disponibilidad: ${mesa.disponibilidad} - Estado: ${mesa.estado}`;
         btnMesa.className = `mesa ${mesa.estado === 'libre' ? 'libre' : 'ocupada'}`;
 
-        // Crear botones para agregar comanda y liberar mesa
-        const btnAgregarComanda = crearBoton('Agregar Comanda', 'btn-comanda', () => agregarComanda(index));
+        // Agregar evento de clic al botón de la mesa para agregar comanda
+        btnMesa.addEventListener('click', () => agregarComanda(index));
+
+        // Crear botón para liberar mesa
         const btnLiberar = crearBoton('Liberar', 'btn-liberar', () => cambiarEstadoLiberar(index));
 
         // Agregar elementos al DOM
         divMesa.appendChild(btnMesa);
-        divMesa.appendChild(btnAgregarComanda);
         divMesa.appendChild(btnLiberar);
 
         contenedorMesas.appendChild(divMesa);
     });
 }
+
 
 // Función para formatear la hora en un formato legible
 function formatTime(date) {
